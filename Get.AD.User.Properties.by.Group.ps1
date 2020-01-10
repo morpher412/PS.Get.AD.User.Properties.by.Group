c310181a89d7dc745e25dfc51486ee13a128eaa7
morpher412 <44172749+morpher412@users.noreply.github.com>
2020-01-10 16:23:15 -0700

    Create Get.AD.User.Properties.by.Group.ps1
    add file to directory

---
+++ Get.AD.User.Properties.by.Group.ps1
@@ -0,0 +1,6 @@
+﻿$DEPTname = #Insert dept name here e.g. "ITProductDevelopment"
+Get-ADGroupMember “$deptname” |
+Where-Object { $_.objectClass -eq 'user' } |
+Get-ADUser -Properties name, emailaddress |
+Select-Object Name, emailaddress |
+Export-csv -path C:\Output\“$deptname”.csv
